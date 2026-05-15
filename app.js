38e617249e59361e0d6655512104cf394b73c42f
Thiago Ferreira <thiagoalvesferreira.taf@gmail.com>
2026-05-15 19:08:52 -0300

    Projeto inicial com testes

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        mensagem: "API funcionando!"
+    });
+});
+
+app.get("/soma", (req, res) => {
+    const a = Number(req.query.a);
+    const b = Number(req.query.b);
+
+    const resultado = a + b;
+
+    res.status(200).json({
+        resultado
+    });
+});
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,29 @@
+const request = require("supertest");
+const app = require("./app");
+
+describe("Testes da API", () => {
+
+    test("Deve retornar API funcionando", async () => {
+        const response = await request(app).get("/");
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.mensagem).toBe("API funcionando!");
+    });
+
+    test("Deve somar corretamente", async () => {
+        const response = await request(app)
+            .get("/soma?a=2&b=3");
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body.resultado).toBe(5);
+    });
+
+    // TESTE COM FALHA PROPOSITAL
+    test("Teste propositalmente errado", async () => {
+        const response = await request(app)
+            .get("/soma?a=2&b=2");
+
+        expect(response.body.resultado).toBe(5);
+    });
+
+});
